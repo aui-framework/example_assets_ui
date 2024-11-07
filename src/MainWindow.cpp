dc56db50ad4a06cf48a5dfb8581a3bcb8981538a
alex2772 <alex2772sc@gmail.com>
2024-11-07 03:24:34 +0300

    added assets

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -3,6 +3,7 @@
 #include <AUI/View/ALabel.h>
 #include <AUI/View/AButton.h>
 #include <AUI/Platform/APlatform.h>
+#include <AUI/View/ADrawableView.h>
 
 using namespace declarative;
 
@@ -10,6 +11,7 @@ MainWindow::MainWindow(): AWindow("Project template app", 300_dp, 200_dp) {
     setContents(
         Centered{
             Vertical{
+                Centered { Icon { ":img/logo.svg" } with_style { FixedSize(64_dp) } },
                 Centered { Label { "Hello world from AUI!" } },
                 _new<AButton>("Visit GitHub repo").connect(&AView::clicked, this, [] {
                     APlatform::openUrl("https://github.com/aui-framework/aui");
